291a4a66cf343a1913b73c58bbda97d9797e71e0
Rico <teh_programerer@interactivelaboratories.com>
2014-03-21 15:42:06 +0000

    |BUILD CONFIGURATION| [Linux] * Added a basic set of build files from ZED-Template   [https://www.github.com/OpenGameDevelopers/ZED-Template.git]
    |SOURCE|
    [Linux]
    * Added a basic set of source files from ZED-Template
      [https://www.github.com/OpenGameDevelopers/ZED-Template.git]

--- Source/Common/Source/Main.cpp
+++ Source/Common/Source/Main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <Game.hpp>
 #include <GitVersion.hpp>
 
 int main( int p_Argc, char **p_ppArgv )
 {
-	std::cout << "Open|Phantasy [Client]" << std::endl;
-	std::cout << "Version: " << GIT_BUILD_VERSION << " [" << GIT_TAG_NAME <<
-		"]" << std::endl;
-	std::cout << "Revision: " << GIT_COMMITHASH << " [" << GIT_COMMITTERDATE <<
-		"]" << std::endl;
-	return 0;
+	std::cout << "OpenPhantasyClient" << std::endl;
+	std::cout << "\tVersion: " << GIT_BUILD_VERSION << std::endl;
+	std::cout << "\tHash: " << GIT_COMMITHASH << std::endl;
+	std::cout << "\tDate: " << GIT_COMMITTERDATE << std::endl;
+	std::cout << "\tTag: " << GIT_TAG_NAME << std::endl;
+	std::cout << "\tBranch: " << GIT_BRANCH << std::endl;
+
+	OpenPhantasyClient::Game TheGame;
+
+	if( TheGame.Initialise( ) != ZED_OK )
+	{
+		std::cout << "[OpenPhantasyClient::main] <ERROR> Failed to initialise the "
+			"game" << std::endl;
+
+		return ZED_FAIL;
+	}
+
+	return TheGame.Execute( );
 }
 
